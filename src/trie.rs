9d37a6d9a7e5300c4bda67e747f4c773289ce9a0
gatsey <felixgate13@gmail.com>
2022-10-17 16:42:04 +0100

    introducing node type and fixing overwriting bug

--- src/trie.rs
+++ src/trie.rs
@@ -1,24 +1,23 @@
 
-
+#[derive(Debug)]
 pub struct ChildNode {
     value: u8,
-    is_end: bool,
     // Boxed due to infinate recursion.
-    nodes: Box<[Option<ChildNode>; 16]>
+    nodes: Box<[Option<ChildNode>; 16]>,
+    node_type: NodeType,
 }
 
 impl ChildNode {
+    // Create a empty which is of type branch.
     fn new(value: u8, is_end: bool) -> ChildNode {
         Self {
             value,
-            is_end,
-            nodes: Box::new([None, None,None, None,None, None,None, None,None, None,None, None,None, None,None, None])
+            nodes: Box::new([None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None]),
+            node_type: if !is_end {NodeType::Branch} else {NodeType::Leaf} ,
         }
     }
 
     fn insert(&mut self, key: &[u8], data: &[u8]) -> Result<(), ()> {
-        let mut has_found_node: bool = false;
-
         // Check to see if the node exists in the children.
         if  self.nodes[key[0] as usize].is_some() {
             // If the key is not at the end then see if we can find a deeper node.
@@ -28,14 +27,10 @@ impl ChildNode {
                 .expect("node has been found; qed")
                 .insert(&key[1..], data)?;
             } else {
-                // Set child node as leaf.
+                // We have found the leaf node and must go no further. 
             }
-            // This is required so after recursion is complete we don't override the existing data.
-            has_found_node = true;
-        }
-
-        // Alas if we have not found a node then we must create a new node full of None bar the node we need.
-        if !has_found_node {
+        } else {
+        // Alas if we have not found a node then we must create our new recursive node and keep the existing data.
             let mut new_node = ChildNode::new( 
                 key[0],
                 key.len() == 1
@@ -43,18 +38,18 @@ impl ChildNode {
             // Again we must continue recursion on the new node until key is len 1.
             if key.len() != 1 {
                 let _ = new_node.insert(&key[1..], data)?;
-            } else {
-                // set child node as leaf.
             }
             // Here we instantiate the new optional nodes and set the key to the new node.
             // This is done after recursion because we need that node to be populated with the nested nodes within it.
-            let mut nodes: [Option<ChildNode>; 16] = [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,];
-            nodes[key[0] as usize] = Some(new_node);
-            self.nodes = Box::new(nodes);
+            self.nodes[key[0] as usize] = Some(new_node);
         }
-
-        Ok(())
     }
+        Ok(())
+}
+
+pub enum NodeType {
+    Leaf,
+    Branch,
 }
 
 pub struct Trie {
@@ -80,4 +75,23 @@ impl Trie {
     fn remove(key: &[u8]) -> Result<(), ()> {
         Ok(())
     }
+
+    // An idea to add patricia trie optimisation.
+    fn optimise_patricia() -> (){
+        todo!()
+    }
+}
+
+
+// current issues is that when inserting 122 it overrides 123
+#[test]
+fn test_insert_state() {
+    let mut Trie = Trie::new();
+
+    assert!(Trie.insert(&[1,2,3], &[0]).is_ok());
+    assert!(Trie.insert(&[1,2,2,3], &[0]).is_ok());
+    dbg!(&Trie.root);
+
+    assert!(Trie.insert(&[1,2], &[0]).is_err());
+
 }
