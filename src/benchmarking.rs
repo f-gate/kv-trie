c631391ce1a9ad10c6e262224f410be4f0eae124
gatsey <felixgate13@gmail.com>
2022-10-27 16:20:32 +0100

    moved benchmarking fns and added the crazy benchmark

--- src/benchmarking.rs
+++ src/benchmarking.rs
@@ -1,23 +1,55 @@
 
-#[cfg(test)]
-mod tests {
     use super::*;
     use crate::Trie;
     use blake2::Blake2b512;
-
-    #[test]
-    fn bench_add_two() {
+    use core::time::Duration;
+    pub fn bench_get() {
         let mut trie: Trie<Blake2b512, u64, &str> = Trie::new();
-
-        use std::time::Instant;
-            let now = Instant::now();
-
-    {
         trie.insert("hello_world !! 12345", 60u64);
+        use std::time::Instant;
+        let now = Instant::now();
+    {
+        trie.get("hello_world !! 12345");
     }
-
+    
     let elapsed = now.elapsed();
-        println!("Elapsed: {:.2?}", elapsed);
+        println!("Get() elapsed: {:.2?}", elapsed);
+    }
+        
+    pub fn bench_insert() {
+        let mut trie: Trie<Blake2b512, u64, &str> = Trie::new();
+        use std::time::Instant;
+        let now = Instant::now();
+        {
+            trie.insert("hello_world !! 12345", 60u64);
+        }
+        let elapsed = now.elapsed();
+        println!("Insert() elapsed: {:.2?}", elapsed);
+
+        let now = Instant::now();
+        {
+            trie.insert("hello_world !! 12345", 60u64);
+        }
+        let elapsed = now.elapsed();
+        println!("Second insert to same key() elapsed: {:.2?}", elapsed);
     }
 
-}
+
+    pub fn insert_crazy() {
+        let mut trie: Trie<Blake2b512, u64, &str> = Trie::new();
+        use std::time::Instant;
+        let now = Instant::now();
+        let input: Vec<String> = (0..10000).into_iter().map(|i| {
+            i.to_string()
+        }).collect();
+        {
+            (0..10000u64).into_iter().map(|i| {
+
+                trie.insert(&input[i as usize].as_str(), 100000000u64);
+            }).collect::<_>()
+            
+        }
+        let elapsed = now.elapsed();
+        println!("insert crazy average elapsed: {:.2?}", elapsed/10000u32);
+
+    }
--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,9 @@ use trie::*;
 use blake2::Blake2b512;
 
 fn main() {
-    bench_insert();
-    bench_get();
-}
-
-  
-fn bench_get() {
-    let mut trie: Trie<Blake2b512, u64, &str> = Trie::new();
-    trie.insert("hello_world !! 12345", 60u64);
-
-    use std::time::Instant;
-        let now = Instant::now();
-
-{
-    trie.get("hello_world !! 12345");
-}
-
-let elapsed = now.elapsed();
-    println!("Get() elapsed: {:.2?}", elapsed);
+    benchmarking::bench_insert();
+    benchmarking::bench_get();
+    benchmarking::insert_crazy();
 }
 
 
-    
-    fn bench_insert() {
-        let mut trie: Trie<Blake2b512, u64, &str> = Trie::new();
-
-        use std::time::Instant;
-            let now = Instant::now();
-
-    {
-        trie.insert("hello_world !! 12345", 60u64);
-    }
-
-    let elapsed = now.elapsed();
-        println!("Insert() elapsed: {:.2?}", elapsed);
-    }
