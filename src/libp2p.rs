d91aa6df820d07ff6aac34cdfd4bb54bc02f9418
Gatsey <felixgate13@gmail.com>
2022-11-15 13:28:15 +0000

    successfully modified the trie to be compatible with libp2p

--- src/libp2p.rs
+++ src/libp2p.rs
@@ -1,3 +1,9 @@
+/// An example on how to integrate your own data structure into libp2p's Kademlia protocol.
+/// This Memory store trait is passed into the Kademlia which in turn is used
+/// when instantiating the swarm.
+/// Unfortunatly the methods provided by RecordStore are not very generic and require
+/// alot of fiddeling when using a generic heay structure like my trie.
+/// In some cases it seems neccesary to remove much of my generic implementation all together :(
 
 extern crate alloc;
 
@@ -11,8 +17,12 @@ use libp2p::kad::{
 use crate::trie::*;
 use alloc::borrow::Cow;
 use std::{iter, collections::{hash_map, hash_set}};
+use blake2::{self, Blake2b512};
+use digest::Digest;
 
-impl<'a ,T: StorageConfig> RecordStore<'a> for Trie<T> {
+impl <'a, T: Sized + Clone>  RecordStore<'a> for Trie<T> 
+where Trie<T>: StorageMethod<Record, Vec<u8>, Blake2b512>
+{
     //From libp2p repo: rust-libp2p/protocols/kad/src/record/store/memory.rs
     // Modified to used a Vec.
     type RecordsIter =
@@ -24,13 +34,30 @@ impl<'a ,T: StorageConfig> RecordStore<'a> for Trie<T> {
     >;
    
     fn get(&self, k: &libp2p::kad::record::Key) -> Option<std::borrow::Cow<'_, libp2p::kad::Record>> {
-        let result = <Self as StorageMethod<T>>::get(&self, k.to_vec().as_slice());
-
-        None
+//
+        let result = <Self as StorageMethod<Record, Vec<u8>, Blake2b512>>::get(&self, k.to_vec());
+        if let Ok(data) = result {
+            Some(Cow::Owned(
+                Record {
+                    key: k.clone(),
+                    //todo
+                    value: vec![],
+                    publisher: None,
+                    expires: None,
+                    }
+                )
+            )
+        } else {
+            // Handle Error?
+            None
+        }
     }
 
     fn put(&mut self, r: libp2p::kad::Record) -> libp2p::kad::store::Result<()> {
-        todo!()
+
+        <Self as StorageMethod<Record, Vec<u8>, Blake2b512>>::insert(self, r.key.to_vec(), r);
+
+        return Ok(())
     }
 
     fn remove(&mut self, k: &libp2p::kad::record::Key) {
@@ -57,5 +84,3 @@ impl<'a ,T: StorageConfig> RecordStore<'a> for Trie<T> {
         todo!()
     }
 }
-
-
--- src/trie.rs
+++ src/trie.rs
@@ -1,32 +1,29 @@
 
 use digest::*;
 use blake2::*;
-use core::marker::PhantomData;
-
+use libp2p::kad::Record;
 use crate::childnode::ChildNode;
 use crate::utils::*;
 
-pub trait StorageConfig {
-    type Key: Sized;
-    type Hasher: Digest;
-    type Data: Sized + Clone;
-}
 
-pub trait StorageMethod<T: StorageConfig> {
-    fn get(&self, key: T::Key) -> Result<T::Data, TrieError>;
-    fn insert(&mut self, key: T::Key, data: T::Data) -> Result<(), ()>;
-    fn remove(&mut self, key: T::Key) -> Result<bool, TrieError>;
+pub trait StorageMethod<T: Sized + Clone, K: Sized, I: Digest> {
+
+    fn get(&self, key: K) -> Result<T, TrieError>;
+    fn get_from_slice(&self, key: &[u8]) -> Result<T, TrieError>;
+    fn insert(&mut self, key: K, data: T) -> Result<(), ()>;
+    fn remove(&mut self, key: K) -> Result<bool, TrieError>;
     fn new() -> Self;
 }
 
 /// The trie, currently programmed as base 16.
 /// Uses the hash on the key inputted to compute a place in storage.
 #[derive(Debug)]
-pub struct Trie<T: StorageConfig> {
-    pub root: ChildNode<T::Data>,
+pub struct Trie<T: Sized + Clone> {
+    pub root: ChildNode<T>,
 }
 
-impl<T: StorageConfig> StorageMethod<T> for Trie<T>{
+impl <T: Sized + Clone, K: Sized, I: Digest> StorageMethod<T, K, I> for Trie<T>{
+
     fn new() -> Self {
         Self {
             root: ChildNode::new_branch(0),
@@ -34,8 +31,8 @@ impl<T: StorageConfig> StorageMethod<T> for Trie<T>{
     }
 
     /// Insert an item into the trie.
-    fn insert(&mut self, key: T::Key, data: T::Data) -> Result<(), ()> {
-        let hash_bytes = hash_me::<T::Hasher, T::Key>(key);
+    fn insert(&mut self, key: K, data: T) -> Result<(), ()> {
+        let hash_bytes = hash_me::<I, K>(key);
         // Compute the "decimal index representation of hex", a necessary evil for the behaivour of the hex trie .
         let index_representation = get_index_rep_of_hex(hash_bytes.as_slice());
 
@@ -44,18 +41,23 @@ impl<T: StorageConfig> StorageMethod<T> for Trie<T>{
             &data,
         )  
     }
+    /// Get an item from the trie.
+    fn get_from_slice(&self, key: &[u8]) -> Result<T, TrieError> {
+        let index_of_hex = get_index_rep_of_hex(key);
+        self.root.get(index_of_hex.as_slice())
+    }
 
     /// Get an item from the trie.
-    fn get(&self, key: T::Key) -> Result<T::Data, TrieError> {
-        let hash_bytes = hash_me::<T::Hasher, T::Key>(key);
+    fn get(&self, key: K) -> Result<T, TrieError> {
+        let hash_bytes = hash_me::<I, K>(key);
 
         let index_of_hex = get_index_rep_of_hex(hash_bytes.as_slice());
         self.root.get(index_of_hex.as_slice())
     }
 
     /// Remove an item from the trie.
-    fn remove(&mut self, key: T::Key) -> Result<bool, TrieError> {
-        let hash_bytes = hash_me::<T::Hasher, T::Key>(key);
+    fn remove(&mut self, key: K) -> Result<bool, TrieError> {
+        let hash_bytes = hash_me::<I, K>(key);
 
         let index_of_hex = get_index_rep_of_hex(hash_bytes.as_slice());
         self.root.remove(index_of_hex.as_slice(), index_of_hex.len())
@@ -74,12 +76,12 @@ impl<T: StorageConfig> StorageMethod<T> for Trie<T>{
 //     }).collect();
 //     let _ = (0..max).into_iter().map(|i| {
 //         let _ = trie.insert(input[i as usize].as_str(), i);
-//     }).collect::<()>();
+//     }).collecSelf::<()>();
     
 //     let _ = (0..max).into_iter().map(|i| {
 //         let res = trie.get(input[i as usize].as_str());
 //         assert_eq!(res, Ok(i));
-//     }).collect::<()>();
+//     }).collecSelf::<()>();
 // }
 
 // #[test]
@@ -113,7 +115,7 @@ impl<T: StorageConfig> StorageMethod<T> for Trie<T>{
 
 //         let res = trie.get(input[i as usize].as_str()); 
 //         assert!(res.is_err());
-//     }).collect::<()>();
+//     }).collecSelf::<()>();
 // }
 
 
